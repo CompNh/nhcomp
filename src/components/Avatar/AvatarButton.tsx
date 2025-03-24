de734a2e3bc1407ceaec379d024d7d92da53019a
CompNh <saramanda21c@naver.com>
2025-03-24 14:31:17 +0900

    0.2.11

--- src/components/Avatar/AvatarButton.tsx
+++ src/components/Avatar/AvatarButton.tsx
@@ -1,33 +1,33 @@
-import { ThemeProvider } from "styled-components";
-import { ButtonProps } from "../Button/Button";
-import { GlobalStyle, theme } from "../../styles/theme";
-import { BaseButton } from "../CommonStyle";
+import React, { forwardRef } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { ButtonProps } from '../Button/Button';
+import { GlobalStyle, theme } from '../../styles/theme';
+import { BaseButton } from '../CommonStyle';
 
-const AvartarButton = ({
-    apply, 
-    children, 
-    ...props 
-} : ButtonProps) =>{
+const AvartarButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ apply, children, ...props }, ref) => {
     return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <BaseButton
-                {...props}
-                style={{
-                    width: '2rem',
-                    height: '2rem',
-                    borderRadius: '9999px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    cursor: 'pointer',
-                    ...props.style,           
-                }}
-            >
-                {children}
-            </BaseButton>         
-        </ThemeProvider>               
-    )
-}
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BaseButton
+          ref={ref}
+          {...props}
+          style={{
+            width: '2rem',
+            height: '2rem',
+            borderRadius: '9999px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            cursor: 'pointer',
+            ...props.style,
+          }}
+        >
+          {children}
+        </BaseButton>
+      </ThemeProvider>
+    );
+  }
+);
 
 export default AvartarButton;
