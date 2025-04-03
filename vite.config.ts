c64fd078fbf1ce2daaf6167806e6cf5d633083c0
CompNh <saramanda21c@naver.com>
2025-04-03 13:49:25 +0900

    0.2.20

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,17 @@ export default defineConfig({
     }),
     
   ],
+  resolve: {
+    alias: {
+      'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['styled-components'],
+  },
+  define: {
+    'process.env': {},
+  },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true
